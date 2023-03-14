ba9652a08c0dd354434ce7afcf9b3dbf8a25e276
Jacob <Jacobshushanyan@icloud.com>
2023-03-14 22:22:12 +0400

    Profile Demo

---
+++ deSmet/View/Profile/ProfileContent.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileContent.swift
+//  deSmet
+//
+//  Created by Apple on 3/13/23.
+//
+
+import SwiftUI
+
+struct ProfileContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileContent_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileContent()
+    }
+}
